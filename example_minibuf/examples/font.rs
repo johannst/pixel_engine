cd8eb06aaf78d2c71833c0326a11ba7bfd9e93c4
johannst <johannes.stoelp@gmail.com>
2019-09-14 07:00:45 +0200

    added B,C,D,E,F letter sprites for testing

--- example_minibuf/examples/font.rs
+++ example_minibuf/examples/font.rs
@@ -18,13 +18,23 @@ fn main() {
 
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
-    for (l, line) in pixel_engine::font::get_sprite('A').lines().enumerate() {
-        for (c, &pixel) in line.iter().enumerate() {
-            if pixel == pixel_engine::Pixel::C {
-                buffer[l * WIDTH + c] = 0x00ff_ffff;
+    let mut render = |x, letter| {
+        for (l, line) in pixel_engine::font::get_sprite(letter).lines().enumerate() {
+            for (c, &pixel) in line.iter().enumerate() {
+                if pixel == pixel_engine::Pixel::C {
+                    buffer[l * WIDTH + c + x] = 0x00ff_ffff;
+                }
             }
         }
-    }
+    };
+    render(0, 'D');
+    render(8, 'E');
+    render(16, 'A');
+    render(24, 'D');
+    render(32, 'B');
+    render(40, 'E');
+    render(48, 'E');
+    render(56, 'F');
 
     window.update_with_buffer(&buffer).unwrap();
 
--- src/font.rs
+++ src/font.rs
@@ -14,9 +14,79 @@ const SPRITE_A: Sprite = Sprite {
     ],
 };
 
+const SPRITE_B: Sprite = Sprite {
+    lines: [
+        [U, U, U, U, U, U, U, U],
+        [U, C, C, C, C, C, U, U],
+        [U, C, U, U, U, U, C, U],
+        [U, C, C, C, C, C, U, U],
+        [U, C, U, U, U, U, C, U],
+        [U, C, U, U, U, U, C, U],
+        [U, C, C, C, C, C, U, U],
+        [U, U, U, U, U, U, U, U],
+    ],
+};
+
+const SPRITE_C: Sprite = Sprite {
+    lines: [
+        [U, U, U, U, U, U, U, U],
+        [U, C, C, C, C, C, C, U],
+        [U, C, U, U, U, U, U, U],
+        [U, C, U, U, U, U, U, U],
+        [U, C, U, U, U, U, U, U],
+        [U, C, U, U, U, U, U, U],
+        [U, C, C, C, C, C, C, U],
+        [U, U, U, U, U, U, U, U],
+    ],
+};
+
+const SPRITE_D: Sprite = Sprite {
+    lines: [
+        [U, U, U, U, U, U, U, U],
+        [U, C, C, C, C, C, U, U],
+        [U, C, U, U, U, U, C, U],
+        [U, C, U, U, U, U, C, U],
+        [U, C, U, U, U, U, C, U],
+        [U, C, U, U, U, U, C, U],
+        [U, C, C, C, C, C, U, U],
+        [U, U, U, U, U, U, U, U],
+    ],
+};
+
+const SPRITE_E: Sprite = Sprite {
+    lines: [
+        [U, U, U, U, U, U, U, U],
+        [U, C, C, C, C, C, C, U],
+        [U, C, U, U, U, U, U, U],
+        [U, C, C, C, C, C, C, U],
+        [U, C, U, U, U, U, U, U],
+        [U, C, U, U, U, U, U, U],
+        [U, C, C, C, C, C, C, U],
+        [U, U, U, U, U, U, U, U],
+    ],
+};
+
+const SPRITE_F: Sprite = Sprite {
+    lines: [
+        [U, U, U, U, U, U, U, U],
+        [U, C, C, C, C, C, C, U],
+        [U, C, U, U, U, U, U, U],
+        [U, C, C, C, C, C, C, U],
+        [U, C, U, U, U, U, U, U],
+        [U, C, U, U, U, U, U, U],
+        [U, C, U, U, U, U, U, U],
+        [U, U, U, U, U, U, U, U],
+    ],
+};
+
 pub fn get_sprite(letter: char) -> &'static Sprite {
     match letter {
         'A' => &SPRITE_A,
+        'B' => &SPRITE_B,
+        'C' => &SPRITE_C,
+        'D' => &SPRITE_D,
+        'E' => &SPRITE_E,
+        'F' => &SPRITE_F,
         _ => {
             unimplemented!();
         }
